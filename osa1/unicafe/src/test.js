035ebb14e472399db8f8b656053748c7259a5e19
coodeex <juhani.s.lehto@gmail.com>
2020-04-22 23:06:58 +0300

    1.6

---
+++ osa1/unicafe/src/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heloust = ({name}) => {
+  // console.log(name)
+
+  return (
+  <div>Morrjesta {name}</div>
+  )
+}
+
+export default Heloust
--- osa1/unicafe/src/test2.js
+++ osa1/unicafe/src/test2.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
 
-const App = () => {
+
+
+// const Button = ({ handleClick, text }) => (
+//   <button onClick={handleClick}>
+//     {text}
+//   </button>
+// )
+
+
+const Hook = (props) => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -36,9 +44,42 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+
+
+
+
+
+
+
+
+
+
+// const Hook = (props) => {
+//   const [ counter, setCounter ] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+  
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={increaseByOne}>
+//         plus
+//       </button>
+//       <button onClick={setToZero}>
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
+
+export default Hook
+
+
+
+
+
 
 
 
