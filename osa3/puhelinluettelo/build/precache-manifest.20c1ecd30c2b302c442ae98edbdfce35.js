c1bf2e285309b928245b0e90649edbb180ab764a
coodeex <juhani.s.lehto@gmail.com>
2020-07-01 23:58:12 +0300

    build

---
+++ osa3/puhelinluettelo/build/precache-manifest.20c1ecd30c2b302c442ae98edbdfce35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40341a254c3eab895197913cb200cdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a3600c1dc9b90d5fa2",
+    "url": "/static/js/2.4baeefc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4baeefc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84149b579131d9e7e13",
+    "url": "/static/js/main.1a096ef4.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
