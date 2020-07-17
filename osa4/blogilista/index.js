12e320c341e79a0178b4c702a3596ab895986de7
coodeex <juhani.s.lehto@gmail.com>
2020-07-17 12:15:29 +0300

    refactor index.js, separate app.js

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,26 +1,10 @@
-// const app = require('./app')
+const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// const server = http.createServer(app)
+const server = http.createServer(app)
 
-const express = require('express')//
-const app = express()//
-const cors = require('cors')//
-const mongoose = require('mongoose')//
-const blogsRouter = require('./controllers/blogs')//
-
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
