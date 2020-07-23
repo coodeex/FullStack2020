9997c2b00007112d0d87cea7e0593e63765cd775
coodeex <juhani.s.lehto@gmail.com>
2020-07-23 14:39:43 +0300

    4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -13,9 +13,16 @@ test('blogs are returned as json', async () => {
 
 test('of number of blogs is right', async () => {
   const response = await api.get('/api/blogs')
+  // console.log(response)
   expect(response.body).toHaveLength(2)
 })
 
+
+test('blog identifier is id', async () => {
+  const response = await api.get('/api/blogs')
+  console.log("\n\n\niidee\n\n",response.body[0].id)
+  expect(response.body[0].id).toBeDefined
+})
 
 afterAll(() => {
   mongoose.connection.close()
